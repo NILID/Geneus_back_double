b3eb086d239846fe7ee7ee4beff171fc16ecdd4a
Dmitry Ilin <dm.ilin@mail.ru>
2026-04-29 22:49:46 +0300

    remove unused code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,2 @@
 module ApplicationHelper
-
-  # show value if exist
-  # or text EMPTY with css class equal text-muted
-  def show_or_empty(value)
-    value.blank? ? content_tag(:span, 'Empty', class: 'text-muted') : value
-  end
 end
