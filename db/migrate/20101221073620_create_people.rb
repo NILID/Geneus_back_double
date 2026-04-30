3b0ba6d75b992b752dc149aeb1d9b69fbbcfa0ed
Dmitry Ilin <dm.ilin@mail.ru>
2026-05-01 00:19:29 +0300

    Remove old migrations

--- db/migrate/20101221073620_create_people.rb
+++ db/migrate/20101221073620_create_people.rb
@@ -1,7 +1,9 @@
 class CreatePeople < ActiveRecord::Migration[5.2]
   def change
     create_table :people do |t|
-      t.string :name
+      t.string :first_name, default: nil
+      t.string :last_name, default: nil
+
       t.string :gender
       t.text :bio
       t.date :date_of_birth
